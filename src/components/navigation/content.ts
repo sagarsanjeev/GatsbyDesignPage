fe10728bf8247c5e6bb48ed11be4703f96f6a64b
Sagar Sanjeev <1405416@kiit.ac.in>
2022-08-22 17:54:45 +0530

    CHanges

--- src/components/navigation/content.ts
+++ src/components/navigation/content.ts
@@ -1,6 +1,5 @@
 const options = [
   { to: '/', text: 'Home' },
-  { to: '/homepage/', text: 'Homepage' },
   { to: '/about/', text: 'About' },
   { to: '/experience/', text: 'Experience' },
   { to: '/projects/', text: 'Projects' },
